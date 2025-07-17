f85478852948fabf87d77dbd685e7daa500e0e10
hafriz <hafriz@gmail.com>
2025-07-17 12:49:19 +0800

    Refactor documentation structure by moving index and completion web services files to the content directory and updating Pelican configuration path

--- docs/pelicanconf.py
+++ docs/pelicanconf.py
@@ -2,6 +2,6 @@ AUTHOR = 'Bahagian Pembelajaran Digital'
 SITENAME = 'e-Learning Platform Services Documentation'
 SITEURL = ''
 
-PATH = 'content'
+PATH = 'docs/content'
 TIMEZONE = 'Asia/Kuala_Lumpur'
 DEFAULT_LANG = 'en'
