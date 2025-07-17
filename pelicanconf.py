7493734dfeceb2a4d9425bd4dc5e1da21cc000b4
hafriz <hafriz@gmail.com>
2025-07-17 12:44:07 +0800

    Refactor documentation structure and update build configuration for Pelican

---
+++ pelicanconf.py
@@ -0,0 +1,7 @@
+AUTHOR = 'Bahagian Pembelajaran Digital'
+SITENAME = 'e-Learning Platform Services Documentation'
+SITEURL = ''
+
+PATH = 'content'
+TIMEZONE = 'Asia/Kuala_Lumpur'
+DEFAULT_LANG = 'en'
